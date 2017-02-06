c1b606f2610110e51096a321f63589506df48eaf
Jhovany Gonzalez <jhovanygonzalez@Jhovanys-MacBook-Pro-2.local>
2017-02-06 10:10:27 -0600

    create a super class enemy and and minion class

--- enemy.py
+++ enemy.py
@@ -1,12 +1,35 @@
 
 
 from utils import *
+
 class Enemy:
 
-    def __init__(self, filename, pos, vel,health, attack):
-        self.filename = filename
+    def __init__(self, name, attack, health):
+
         self.health = health
         self.attack = attack
-        self.pos = vector2(pos.x,pos.y)
+        self.name = name
+
+    def is_Alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
+
+    def returnHealth(self):
+        if self.is_Alive() == True:
+            return self.health
+
+
+class Minion(Enemy):
+
+    def __init__(self,filename, pos, vel):
+        self.filename = filename
+        self.pos = vector2(pos.x, pos.y)
         self.vel = vector2(vel.x,vel.y)
+
+        super().__init__(name = "Minion", attack=20, health=100)
+
+
+
 
