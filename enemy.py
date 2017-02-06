352703fa597c824c9d0472a56b199e907c8ade8d
Jhovany Gonzalez <jhovanygonzalez@Jhovanys-MacBook-Pro-2.local>
2017-02-06 09:42:12 -0600

    just adding some stuff in the enemy class

--- enemy.py
+++ enemy.py
@@ -1,6 +1,12 @@
 
 
-
+from utils import *
 class Enemy:
 
-    def __init__(self, filename, x, y, vx, vy,health, attack):
+    def __init__(self, filename, pos, vel,health, attack):
+        self.filename = filename
+        self.health = health
+        self.attack = attack
+        self.pos = vector2(pos.x,pos.y)
+        self.vel = vector2(vel.x,vel.y)
+
