4900a6a78df532308aee59b52932003a2822f3f3
Israel <israel.l.cabello@gmail.com>
2017-02-05 20:09:37 -0600

    Player class
    Adding Player class and some test code to test the class.

---
+++ main.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python2
+
+import pygame
+import sys
+from utils import vector2, sprite, player
+
+# constants
+SCREEN_SIZE = (1024, 700) # W x H
+COLORS = {
+    'red' : (255, 0, 0),
+    'blue' : (0, 0, 255),
+    'green' : (0, 255, 0),
+    'yellow' : (255, 255, 0),
+    'orange' : (255, 165, 0),
+    'pink' : (255, 192, 203),
+    'white' : (255, 255, 255),
+    'black' : (0, 0, 0)
+}
+PLACES = {
+    'topleft' : (0, 0),
+    'topright' : (SCREEN_SIZE[0], 0),
+    'bottomleft' : (0, SCREEN_SIZE[1]),
+    'bottomright' : (SCREEN_SIZE[0], SCREEN_SIZE[1]),
+    'center' : (SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 2)
+}
+WIZARD_FILE = 'art/Wizard_Male.png'
+
+# setup goes here
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("hi there")
+
+accel = vector2(0.025, 0)
+
+sprites = [player(WIZARD_FILE, vector2(100, 100), vector2(0, 0), name="Player")
+           ]
+player = sprites[0]
+
+old_tick = pygame.time.get_ticks()
+
+while True:
+    # get user events
+    pygame.event.pump()
+    for evt in pygame.event.get():
+        if evt.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif evt.type == pygame.KEYDOWN and evt.key == pygame.K_ESCAPE:
+            pygame.quit()
+            sys.exit()
+
+    # simulation stuff goes here (nothing to update yet)
+    current_tick = pygame.time.get_ticks()
+    delta = current_tick - old_tick
+    if delta > 100: # prevent huge spikes in acceletation?
+        old_tick = pygame.time.get_ticks()
+        continue
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_SPACE]:
+        player.shield = True
+    else:
+        player.shield = False
+        
+    if keys[pygame.K_a]: # a is currently pressed
+        player.velocity.x -= 0.002
+    elif keys[pygame.K_d]: # d is currently pressed
+        player.velocity.x += 0.002
+    else:
+        if player.velocity.x > 0:
+            player.velocity.x -= accel.scale(delta).x
+            if player.velocity.x < 0:
+                player.velocity.x = 0
+        elif player.velocity.x < 0:
+            player.velocity.x += accel.scale(delta).x
+            if player.velocity.x > 0:
+                player.velocity.x = 0
+    
+    #accel = accel.scale(delta)
+    for s in sprites:
+        s.update(delta)
+    
+    for i in range(0, len(sprites)):
+        for other in sprites[i + 1:]:
+            sprites[i].collision(other)
+
+    # draw to screen and flip
+    screen.fill(COLORS['yellow'])
+    for s in sprites:
+        s.draw(screen)
+    pygame.display.flip()
+    old_tick = current_tick # pygame.time.get_ticks()
+    
---
+++ utils.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python2
+
+import math
+import pygame
+
+class vector2:
+    # constructor
+    def __init__(self, x, y):
+        # unlike C++?/Java, no implicit "this" pointer
+        # instead, reference is passed in as first argument (self in this case)
+        self.x = x
+        self.y = y
+
+    # method definitions
+    def add(self, other):
+        v = vector2(self.x + other.x, self.y + other.y)
+        return v
+
+    def subtract(self, other):
+        v = vector2(self.x - other.x, self.y - other.y)
+        return v
+
+    def magnitude(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def normalize(self):
+        mag = self.magnitude()
+        v = vector2(self.x / float(mag), self.y / float(mag))
+        return v
+
+    def scale(self, scalar):
+        v = vector2(self.x * scalar, self.y * scalar)
+        return v
+
+    def dot_prod(self, other):
+        return self.x * other.x + self.y * other.y
+
+    def tangent(self):
+        return vector2(self.y, -self.x)
+
+    # overload return-this-as-string for printing
+    def __str__(self):
+        # format allows you to replace "{}" with variable values
+        return '({x}, {y})'.format(x=self.x, y=self.y)
+
+# basic sprite class
+class sprite():
+    def __init__(self, img_fn, pos, vel):
+        self.SS = (1024, 700)
+        self.image = pygame.image.load(img_fn).convert()
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.radius = 75
+        self.position = vector2(pos.x, pos.y)
+        self.velocity = vector2(vel.x, vel.y)
+        self.color = [0, 0, 0]
+
+    def update(self, delta):
+        self.position = self.position.add(self.velocity.scale(delta))
+
+        if self.position.x + self.radius > self.SS[0]: # right wall
+            self.position.x -= (self.radius + self.position.x) - self.SS[0]
+            self.velocity.x = -(self.velocity.x)
+        if self.position.x - self.radius < 0: # left wall
+            self.position.x += self.radius - self.position.x
+            self.velocity.x = -(self.velocity.x)
+        if self.position.y + self.radius > self.SS[1]: # bottom wall
+            self.position.y -= (self.radius + self.position.y) - self.SS[1]
+            self.velocity.y = -(self.velocity.y)
+        if self.position.y - self.radius < 0: # top wall
+            self.position.y += self.radius - self.position.y
+            self.velocity.y = -(self.velocity.y)
+
+    def collision(self, other):
+        cn = self.position.subtract(other.position)
+        dist = cn.magnitude()
+        if dist < (self.radius + other.radius): # collision?
+            scale_factor = 0.5 * ((self.radius + other.radius) - dist)
+            diff = cn.normalize().scale(scale_factor)
+
+            # move balls away from each other
+            self.position = self.position.add(diff)
+            other.position = other.position.subtract(diff)
+
+            # new velocity calculation
+            cn = cn.normalize()
+            ct = cn.tangent()
+
+            dp_ns = self.velocity.dot_prod(cn)
+            dp_no = other.velocity.dot_prod(cn)
+            dp_ts = self.velocity.dot_prod(ct)
+            dp_to = other.velocity.dot_prod(ct)
+
+            vn_s = cn.scale(dp_ns)
+            vn_o = cn.scale(dp_no)
+            vt_s = ct.scale(dp_ts)
+            vt_o = ct.scale(dp_to)
+
+            self.velocity = vt_s.add(vn_o)
+            other.velocity = vt_o.add(vn_s)
+
+            self.color[0] = (self.color[0] + 64) % 256
+            other.color[0] = (other.color[0] + 64) % 256
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (self.color[0], self.color[1], self.color[2]),
+                           (int(self.position.x), int(self.position.y)),
+                           self.radius, 2)
+        screen.blit(self.image, (self.position.x, self.position.y))
+
+class player(sprite):
+    def __init__(self, img_fn, pos, vel, name=None):
+        self.SS = (1024, 700)
+        if name:
+            self.name = name
+        else:
+            self.name = "Minion"
+        self.image = pygame.image.load(img_fn).convert_alpha()
+        self.image_l = pygame.transform.scale(self.image, (75, 75))
+        self.image_r = pygame.transform.flip(self.image_l, True, False)
+        self.image_show = self.image_r
+        self.size = self.image_l.get_size()
+        self.radius = math.sqrt((self.size[0] ** 2) + (self.size[1] ** 2))
+        self.radius = int(self.radius / 2)
+        self.position = vector2(pos.x, pos.y)
+        self.velocity = vector2(vel.x, vel.y)
+        self.color = [0, 0, 0]
+        self.shield = False
+
+    def pic_center(self):
+        size = self.image_show.get_size()
+        center = (int(self.position.x + (size[0] / 2)),
+                  int(self.position.y + (size[1] / 2))
+                  )
+        return center
+
+    def update(self, delta):
+        self.position = self.position.add(self.velocity.scale(delta))
+
+        center = self.pic_center() # 0 = x, 1 = y
+        if self.shield:
+            s_top = center[1] - self.radius
+            s_right = center[0] + self.radius
+            s_bottom = center[1] + self.radius
+            s_left = center[0] - self.radius
+        else:
+            s_top = self.position.y
+            s_right = self.position.x + self.size[0]
+            s_bottom = self.position.y + self.size[1]
+            s_left = self.position.x
+
+        if s_right > self.SS[0]: # right wall
+            self.position.x -= s_right - self.SS[0]
+            #self.velocity.x = -(self.velocity.x)
+        if s_left < 0: # left wall
+            self.position.x += abs(s_left)
+            #self.velocity.x = -(self.velocity.x)
+        if s_bottom > self.SS[1]: # bottom wall
+            self.position.y -= s_bottom - self.SS[1]
+            #self.velocity.y = -(self.velocity.y)
+        if s_top < 0: # top wall
+            self.position.y += abs(s_top)
+            #self.velocity.y = -(self.velocity.y)
+
+        if self.velocity.x > 0:
+            self.image_show = self.image_r
+        elif self.velocity.x < 0:
+            self.image_show = self.image_l
+
+    def collision(self, other):
+        cn = self.position.subtract(other.position)
+        dist = cn.magnitude()
+        if dist < (self.radius + other.radius): # collision?
+            scale_factor = 0.5 * ((self.radius + other.radius) - dist)
+            diff = cn.normalize().scale(scale_factor)
+
+            # move balls away from each other
+            self.position = self.position.add(diff)
+            other.position = other.position.subtract(diff)
+
+            # new velocity calculation
+            cn = cn.normalize()
+            ct = cn.tangent()
+
+            dp_ns = self.velocity.dot_prod(cn)
+            dp_no = other.velocity.dot_prod(cn)
+            dp_ts = self.velocity.dot_prod(ct)
+            dp_to = other.velocity.dot_prod(ct)
+
+            vn_s = cn.scale(dp_ns)
+            vn_o = cn.scale(dp_no)
+            vt_s = ct.scale(dp_ts)
+            vt_o = ct.scale(dp_to)
+
+            self.velocity = vt_s.add(vn_o)
+            other.velocity = vt_o.add(vn_s)
+
+            self.color[0] = (self.color[0] + 64) % 256
+            other.color[0] = (other.color[0] + 64) % 256
+
+    def draw(self, screen):
+        if self.shield:
+            pygame.draw.circle(screen,
+                               (self.color[0], self.color[1], self.color[2]),
+                               self.pic_center(),
+                               self.radius, 2)
+        screen.blit(self.image_show, (self.position.x, self.position.y))
